59818c5abe51569643ceccb01db49abcbbd839da
Emmetion <emmetwormbox6@gmail.com>
2023-08-09 03:20:50 -0400

    Added crafting station

---
+++ src/main/java/me/emmetion/wells/model/CraftableItem.java
@@ -0,0 +1,23 @@
+package me.emmetion.wells.model;
+
+import org.bukkit.Material;
+
+import java.util.List;
+
+public class CraftableSchematic {
+
+    private final String name;
+    private final List<SMaterial> schematicMateiralList;
+
+    public CraftableSchematic(String name, List<SMaterial>, )
+
+
+
+}
+
+class SMaterial {
+
+    private Material;
+    private
+
+}
